a2e8d3681ee80b20ee9e72887b893593ae29dacd
Serhii Kolesnykov <kvs191214@gmail.com>
2025-07-10 23:02:20 +0300

    update connection to database

--- src/main/java/StepApp/DataBase/DbConnection.java
+++ src/main/java/StepApp/DataBase/DbConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DbConnection {
     public static Connection conn() throws SQLException {
        try {
-           String url = System.getenv("DB_URL") != null ? System.getenv("DATABASE_URL") : "jdbc:postgresql://localhost:5432/step_app";
-           String username = System.getenv("DB_USERNAME") != null ? System.getenv("DATABASE_USER") : "postgres";
-           String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DATABASE_PASSWORD") : "<PASSWORD>";
+           String url = System.getenv("DATABASE_URL") != null ? System.getenv("DATABASE_URL") : "jdbc:postgresql://localhost:5432/step_app";
+           String username = System.getenv("DB_USERNAME") != null ? System.getenv("DB_USERNAME") : "postgres";
+           String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "<PASSWORD>";
            System.out.println("Connecting to database: " + url);
            System.out.println("Using username: " + username);
            System.out.println("Using password: " + password);
