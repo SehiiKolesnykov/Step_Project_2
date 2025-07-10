acfe49d886f67fe8104a50fafa726e2b658b47c7
Serhii Kolesnykov <kvs191214@gmail.com>
2025-07-10 22:43:21 +0300

    update connection to db

--- src/main/java/StepApp/DataBase/DbConnection.java
+++ src/main/java/StepApp/DataBase/DbConnection.java
@@ -10,6 +10,9 @@ public class DbConnection {
            String url = System.getenv("DATABASE_URL") != null ? System.getenv("DATABASE_URL") : "jdbc:postgresql://localhost:5432/step_app";
            String username = System.getenv("DB_USERNAME") != null ? System.getenv("DATABASE_USER") : "postgres";
            String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DATABASE_PASSWORD") : "<PASSWORD>";
+           System.out.println("Connecting to database: " + url);
+           System.out.println("Using username: " + username);
+           System.out.println("Using password: " + password);
            return DriverManager.getConnection(url, username, password);
        } catch (SQLException e) {
            throw new SQLException("Error connecting to the database", e);
