1351a638e8702ac4444d084e2dc4dd7c2d77c72c
Serhii Kolesnykov <kvs191214@gmail.com>
2025-07-10 22:19:21 +0300

    update TemplateEngine

--- src/main/java/StepApp/util/TemplateEngine.java
+++ src/main/java/StepApp/util/TemplateEngine.java
@@ -1,15 +1,13 @@
 package StepApp.util;
 
-import StepApp.servlet.TemplateServlet;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,22 +16,12 @@ public class TemplateEngine {
     private final Configuration config;
     private final Map<String, Object> empty = new HashMap<>();
 
-    private String base(String prefix) {
-        try {
-            return TemplateServlet.class
-                    .getClassLoader()
-                    .getResource(prefix)
-                    .toURI()
-                    .getPath();
-
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public TemplateEngine(String prefix) throws IOException {
         this.config = new Configuration(Configuration.VERSION_2_3_33);
-        config.setDirectoryForTemplateLoading(new File(base(prefix)));
+        // Завантажуємо шаблони з ClassLoader (з src/main/resources/templates)
+        config.setClassForTemplateLoading(TemplateEngine.class, "/" + prefix);
+        config.setDefaultEncoding("UTF-8");
+        config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
     }
 
     public void render(String templateName, Map<String, Object> data, HttpServletResponse rs) {
@@ -45,7 +33,7 @@ public class TemplateEngine {
         }
     }
 
-    public void render(String template, HttpServletResponse rs)  {
+    public void render(String template, HttpServletResponse rs) {
         render(template, empty, rs);
     }
 }
